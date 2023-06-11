62a5cbe76ad12d6674b4dbca43a84677afc590b7
Alan <alanfoureyes@gmail.com>
2023-06-12 00:26:57 +0600

    Finish

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+
+export const Button = styled.button`
+    padding: 0.55rem 1rem;
+    background-color: var(--color-ui-base);
+    box-shadow: var(--shadow);
+    line-height: 2.5;
+    border-radius: var(--radii);
+
+    border: none;
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    color: var(--color-text);
+    cursor: pointer;
+
+
+`
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
+import { searchByCountry } from "../config";
+import { Button } from "../components/Button";
+import { Info } from "../components/Info";
 
 export const Details = () => {
-  const { name } = useParams();
+    const navigate = useNavigate();
+    const { name } = useParams();
+    const [ country, setCountry ] = useState(null)
 
-  return (
-    <div>
-      Details: {name}
-    </div>
-  );
+    React.useEffect(() => {
+        axios.get(searchByCountry(name))
+        .then (
+            ({data}) => setCountry(data[0])
+        )
+
+    }, [name])
+  
+    const handleGoBack = () => {
+      // Возврат на предыдущую страницу
+      navigate(-1);
+    };
+  
+    return (
+      <div>
+        <Button onClick={handleGoBack}>
+          <BiArrowBack /> Back
+        </Button>
+        {country && (
+            <Info {...country}/>
+        )}
+        {/* Details: {name} */}
+      </div>
+    );
 };
